01cc0d5feaca21b7ac6ce463a064d23f8e97267b
Matt Ashby <matt.ashby@theinstillery.com>
2020-03-16 11:15:24 +1300

    beginning restructure

--- main.tf
+++ main.tf
@@ -8,6 +8,26 @@ resource "azurerm_resource_group" "firewall" {
 }
 
 # Hub VNet
+resource "azurerm_virtual_network" "region1_firewall" {
+  name                  = element(values(var.fw_vnet_config.vnet_name), 0)
+  location              = var.location
+  resource_group_name   = var.virtual_network_rg
+  address_space         = var.networking_object.vnet.address_space
+  tags                  = local.tags
+
+  dns_servers           = lookup(var.networking_object.vnet, "dns", null)
+
+   dynamic "ddos_protection_plan" {
+    for_each = lookup(var.networking_object.vnet, "enable_ddos_std", false) == true ? [1] : []
+    
+    content {
+      id     = var.networking_object.vnet.ddos_id
+      enable = var.networking_object.vnet.enable_ddos_std
+    }
+  }
+}
+
+
 resource "azurerm_virtual_network" "firewall" {
   count               = var.fw_rg_enabled == true ? length(keys(var.fw_vnet_config.vnet_name)) : 0
   name                = element(values(var.fw_vnet_config.vnet_name), count.index)

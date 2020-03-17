4827672468ac69a4fa5a43601c47be55ba438e4b
Matt Ashby <matt@ashbys.nz>
2020-03-17 13:58:58 +1300

    updated Variables

--- variables.tf
+++ variables.tf
@@ -35,10 +35,6 @@ variable "tags" {
 #   description = "(Required) contains the SA and EH details for operations diagnostics"
 # }
 
-# variable "log_analytics_workspace" {
-#   description = "(Required) contains the log analytics workspace details for operations diagnostics"
-# }
-
 # variable "diagnostics_settings" {
 #   description = "(Required) configuration object describing the diagnostics"
 # }
@@ -56,37 +52,6 @@ variable "vnet_suffix" {
   description = "(Optional) You can use a suffix to add to the list of Virtual Networks you want to create"
   type        = string
 }
-
-# variable "route_tables" {
-#   description = "(Required) Map of the Route Tables to create"
-# }
-
-# Example of resource_groups data structure:
-# route_table = {
-#   name                          = "route_test"
-#   rg                            = "uqvh-HUB-CORE-NET"
-#   location                      = "southeastasia"
-#   disable_bgp_route_propagation = false #optional
-#   route_entries = {
-#     re1 = {
-#       name          = "myroute1"
-#       prefix        = "255.0.0.0/8"
-#       next_hop_type = "None"
-#     },
-#     re2 = {
-#       name                   = "myroute2"
-#       prefix                 = "255.255.0.0/16"
-#       next_hop_type          = "VirtualAppliance"
-#       next_hop_in_ip_address = "192.168.10.5" #required if next_hop_type is "VirtualAppliance"
-#     },
-#     re3 = {
-#       name                   = "defaulroute"
-#       prefix                 = "0.0.0.0/0"
-#       next_hop_type          = "VirtualAppliance"
-#       next_hop_in_ip_address = "192.168.10.5" #required if next_hop_type is "VirtualAppliance"
-#     },
-#   }
-# }
 
 variable "rt_suffix" {
   description = "(Optional) You can use a suffix to add to the list of Route Tables you want to create"
